f8ff158e96d89605c5821f8b47147b0f61df72eb
QuHanSheng <quhansheng@gmail.com>
2018-09-26 18:58:23 +0800

    TODO dynamic draw cat's right hand

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -36,139 +36,13 @@ import java.util.logging.Logger;
 //import java.awt.*;
 
 public class App extends Application implements NativeKeyListener, NativeMouseInputListener {
+    private static final ConcurrentHashMap<String, Image> CACHE = new ConcurrentHashMap<>();
     private static double RESOLUTION_WIDTH;
     private static double RESOLUTION_HEIGHT;
-    private static final ConcurrentHashMap<String, Image> CACHE = new ConcurrentHashMap<>();
     private static ImageView IMAGE_VIEW;
     private static String LEFT_KEY;
     private static String RIGHT_KEY;
     private static Scene scene;
-
-    @Override
-    @SuppressWarnings("Deprecated")
-    public void start(Stage primaryStage) throws IOException, InterruptedException {
-        //Create console textarea
-        TextArea ta = TextAreaBuilder.create().prefWidth(800).prefHeight(400).wrapText(true).editable(false).build();
-        ta.setFont(new Font(15));
-        //This Console class is A class extend the OutputStream, so it can receive the System.out.println() request,IT IS NOT java.io.Console!!!
-        PrintStream ps = new PrintStream(new Console(ta));
-        //Redirect system console IOStream
-        System.setOut(ps);
-        System.setErr(ps);
-
-        //Create window to place console textarea
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Console");
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(ta);
-        Scene scene2 = new Scene(vbox);
-        newWindow.setScene(scene2);
-        // Set position of console window, related to primary window.
-        newWindow.setX(primaryStage.getX() + 200);
-        newWindow.setY(primaryStage.getY() + 100);
-        newWindow.show();
-        // Set On Close Event
-        newWindow.setOnCloseRequest(event -> System.exit(0));
-        newWindow.setResizable(false);
-
-
-        //Set style of main window
-        primaryStage.setTitle("osu! Cat J");
-        Group root = new Group();
-        Scene scene = new Scene(root, 640, 640, Color.WHITE);
-        GridPane gridpane = new GridPane();
-        gridpane.setPadding(new Insets(5));
-        gridpane.setHgap(10);
-        gridpane.setVgap(10);
-        final ImageView imv = new ImageView();
-        final HBox pictureRegion = new HBox();
-        pictureRegion.getChildren().add(imv);
-        gridpane.add(pictureRegion, 1, 1);
-        root.getChildren().add(gridpane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        //set On Close event
-        primaryStage.setOnCloseRequest(event -> System.exit(0));
-        primaryStage.setResizable(false);
-
-        //set the ImageView and Stage to static member to control it later
-        App.IMAGE_VIEW = imv;
-        App.scene = scene;
-        System.out.println( "README: " +
-                "\n - Will NOT work when any application running with admin rights window is in the foreground" +
-                "\n - So do not run osu! in Admin mode;" +
-                "\n - Only tested in Windows 10 and JRE 1.8;");
-
-        System.out.println(Instant.now() + " Starting...");
-
-        initCache();
-        //set icon :3
-        newWindow.getIcons().add(CACHE.get("icon"));
-        primaryStage.getIcons().add(CACHE.get("icon"));
-    }
-
-    public void nativeKeyPressed(NativeKeyEvent e) {
-        if (LEFT_KEY.equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
-            IMAGE_VIEW.setImage(CACHE.get("KeyTapHand1"));
-        }
-        if (RIGHT_KEY.equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
-            IMAGE_VIEW.setImage(CACHE.get("KeyTapHand2"));
-        }
-    }
-
-    public void nativeKeyReleased(NativeKeyEvent e) {
-        if ("Z".equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
-            IMAGE_VIEW.setImage(null);
-        }
-        if ("X".equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
-            IMAGE_VIEW.setImage(null);
-        }
-    }
-
-    @Override
-    public void nativeMouseMoved(NativeMouseEvent nativeMouseEvent) {
-        handleMouseMoveOrDrag(nativeMouseEvent);
-    }
-
-    @Override
-    public void nativeMouseDragged(NativeMouseEvent nativeMouseEvent) {
-        handleMouseMoveOrDrag(nativeMouseEvent);
-    }
-
-    private void handleMouseMoveOrDrag(NativeMouseEvent nativeMouseEvent){
-        int x = nativeMouseEvent.getX();
-        int y = nativeMouseEvent.getY();
-        ImagePattern pattern ;
-        if (x > 0 && y > 0) {
-            if ((x / RESOLUTION_WIDTH) * 3D > 2D) {
-                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
-                    pattern = new ImagePattern(CACHE.get("handI"));
-                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
-                    pattern = new ImagePattern(CACHE.get("handF"));
-                } else {
-                    pattern = new ImagePattern(CACHE.get("handC"));
-                }
-            } else if ((x / RESOLUTION_WIDTH) * 3D > 1D) {
-                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
-                    pattern = new ImagePattern(CACHE.get("handH"));
-                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
-                    pattern = new ImagePattern(CACHE.get("handE"));
-                } else {
-                    pattern = new ImagePattern(CACHE.get("handB"));
-                }
-            } else {
-
-                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
-                    pattern = new ImagePattern(CACHE.get("handG"));
-                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
-                    pattern = new ImagePattern(CACHE.get("handD"));
-                } else {
-                    pattern = new ImagePattern(CACHE.get("handA"));
-                }
-            }
-            scene.setFill(pattern);
-        }
-    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         //Disable jNativeHook's log
@@ -247,11 +121,18 @@ public class App extends Application implements NativeKeyListener, NativeMouseIn
         outPut = IOUtils.toString(process.getInputStream());
         if (outPut.contains("HKEY_LOCAL_MACHINE")) {
             catImagePathPerfix = "\\cat\\tablet\\";
-            System.out.println(Instant.now() + " Tablet detected! loading tablet background image...");
+            System.out.println(Instant.now() + "Third party tablet driver detected! loading tablet background image...");
         } else {
-            //TODO detect Wacom offical driver
-            catImagePathPerfix = "\\cat\\mouse\\";
+            process = Runtime.getRuntime().exec("reg query HKEY_LOCAL_MACHINE\\SOFTWARE\\Wacom");
+            outPut = IOUtils.toString(process.getInputStream());
+            if (outPut.contains("HKEY_LOCAL_MACHINE")) {
+                catImagePathPerfix = "\\cat\\tablet\\";
+                System.out.println(Instant.now() + " Official Wacom tablet driver detected! loading tablet background image...");
+            } else {
+                catImagePathPerfix = "\\cat\\mouse\\";
+            }
         }
+        Image hand = new Image(new File(jarFilePath + catImagePathPerfix + "Hand.png").toURI().toURL().toString(), false);
         Image handA = new Image(new File(jarFilePath + catImagePathPerfix + "Hand A.png").toURI().toURL().toString(), false);
         Image handB = new Image(new File(jarFilePath + catImagePathPerfix + "Hand B.png").toURI().toURL().toString(), false);
         Image handC = new Image(new File(jarFilePath + catImagePathPerfix + "Hand C.png").toURI().toURL().toString(), false);
@@ -261,6 +142,7 @@ public class App extends Application implements NativeKeyListener, NativeMouseIn
         Image handG = new Image(new File(jarFilePath + catImagePathPerfix + "Hand G.png").toURI().toURL().toString(), false);
         Image handH = new Image(new File(jarFilePath + catImagePathPerfix + "Hand H.png").toURI().toURL().toString(), false);
         Image handI = new Image(new File(jarFilePath + catImagePathPerfix + "Hand I.png").toURI().toURL().toString(), false);
+        CACHE.put("hand", hand);
         CACHE.put("handA", handA);
         CACHE.put("handB", handB);
         CACHE.put("handC", handC);
@@ -273,6 +155,145 @@ public class App extends Application implements NativeKeyListener, NativeMouseIn
         //It seems javafx doesn't support .ico ...
         Image icon = new Image(new File(jarFilePath + "\\cat\\cat.ico").toURI().toURL().toString(), false);
         CACHE.put("icon", handI);
+    }
+
+    @Override
+    @SuppressWarnings("Deprecated")
+    public void start(Stage primaryStage) throws IOException, InterruptedException {
+        //Create console textarea
+        TextArea ta = TextAreaBuilder.create().prefWidth(800).prefHeight(400).wrapText(true).editable(false).build();
+        ta.setFont(new Font(15));
+        //This Console class is A class extend the OutputStream, so it can receive the System.out.println() request,IT IS NOT java.io.Console!!!
+        PrintStream ps = new PrintStream(new Console(ta));
+        //Redirect system console IOStream
+        System.setOut(ps);
+        System.setErr(ps);
+
+        //Create window to place console textarea
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Console");
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(ta);
+        Scene scene2 = new Scene(vbox);
+        newWindow.setScene(scene2);
+        // Set position of console window, related to primary window.
+        newWindow.setX(primaryStage.getX() + 200);
+        newWindow.setY(primaryStage.getY() + 100);
+        newWindow.show();
+        // Set On Close Event
+        newWindow.setOnCloseRequest(event -> System.exit(0));
+        newWindow.setResizable(false);
+
+
+        //Set style of main window
+        primaryStage.setTitle("osu! Cat J");
+        Group root = new Group();
+        Scene scene = new Scene(root, 640, 640, Color.WHITE);
+        GridPane gridpane = new GridPane();
+        gridpane.setPadding(new Insets(5));
+        gridpane.setHgap(10);
+        gridpane.setVgap(10);
+        final ImageView imv = new ImageView();
+        final HBox pictureRegion = new HBox();
+        pictureRegion.getChildren().add(imv);
+        gridpane.add(pictureRegion, 1, 1);
+        root.getChildren().add(gridpane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //set On Close event
+        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        primaryStage.setResizable(false);
+
+        //set the ImageView and Stage to static member to control it later
+        App.IMAGE_VIEW = imv;
+        App.scene = scene;
+        System.out.println("README: " +
+                "\n - Will NOT work when any application running with admin rights window is in the foreground" +
+                "\n - So do not run osu! in Admin mode;" +
+                "\n - Only tested in Windows 10 and JRE 1.8;");
+
+        System.out.println(Instant.now() + " Starting...");
+
+        initCache();
+        //set icon :3
+        newWindow.getIcons().add(CACHE.get("icon"));
+        primaryStage.getIcons().add(CACHE.get("icon"));
+    }
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        if (LEFT_KEY.equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
+            IMAGE_VIEW.setImage(CACHE.get("KeyTapHand1"));
+        }
+        if (RIGHT_KEY.equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
+            IMAGE_VIEW.setImage(CACHE.get("KeyTapHand2"));
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        if ("Z".equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
+            IMAGE_VIEW.setImage(null);
+        }
+        if ("X".equals(NativeKeyEvent.getKeyText(e.getKeyCode()))) {
+            IMAGE_VIEW.setImage(null);
+        }
+    }
+
+    @Override
+    public void nativeMouseMoved(NativeMouseEvent nativeMouseEvent) {
+        handleMouseMoveOrDrag(nativeMouseEvent);
+    }
+
+    @Override
+    public void nativeMouseDragged(NativeMouseEvent nativeMouseEvent) {
+        handleMouseMoveOrDrag(nativeMouseEvent);
+    }
+
+    private void handleMouseMoveOrDrag(NativeMouseEvent nativeMouseEvent) {
+        int x = nativeMouseEvent.getX();
+        int y = nativeMouseEvent.getY();
+        int[] coordinate = convert(x, y);
+
+        ImagePattern pattern;
+        if (x > 0 && y > 0) {
+            if ((x / RESOLUTION_WIDTH) * 3D > 2D) {
+                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
+                    pattern = new ImagePattern(CACHE.get("handI"));
+                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
+                    pattern = new ImagePattern(CACHE.get("handF"));
+                } else {
+                    pattern = new ImagePattern(CACHE.get("handC"));
+                }
+            } else if ((x / RESOLUTION_WIDTH) * 3D > 1D) {
+                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
+                    pattern = new ImagePattern(CACHE.get("handH"));
+                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
+                    pattern = new ImagePattern(CACHE.get("handE"));
+                } else {
+                    pattern = new ImagePattern(CACHE.get("handB"));
+                }
+            } else {
+
+                if ((y / RESOLUTION_HEIGHT) * 3D > 2D) {
+                    pattern = new ImagePattern(CACHE.get("handG"));
+                } else if ((y / RESOLUTION_HEIGHT) * 3D > 1D) {
+                    pattern = new ImagePattern(CACHE.get("handD"));
+                } else {
+                    pattern = new ImagePattern(CACHE.get("handA"));
+                }
+            }
+            scene.setFill(pattern);
+        }
+    }
+
+    private int[] convert(int x, int y) {
+        double percentWidth = (x / RESOLUTION_WIDTH);
+        double percentHeight = (y / RESOLUTION_HEIGHT);
+        //求出手掌的坐标
+        int x1 = 195 - (int) (125d * percentWidth) + (int) (60d * percentHeight) + 20;
+        int y1 = 420 - (int) (35d * percentWidth) - (int) (20 * percentHeight) - 20;
+        //394 162 - 374 182
+//        QuadCurve2D curve1 = new QuadCurver2D.Double(20,10,90,65,55,115);
+        return new int[2];
     }
 
     @Override
